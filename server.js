3697a772203ae02e5c110413295dbd3defc8351c
Muqeet466 <mohammeda@deakin.edu.au>
2022-04-27 12:49:51 +1000

    commit

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const MongoClient = require('mongodb').MongoClient;
 let projectCollection;
 
 // Database Connection
-
 const uri = "mongodb+srv://muqeet:muqeet123@cluster1.3ic2y.mongodb.net/sit725_2022_t1?retryWrites=true&w=majority";
-const client = new MongoClient(uri,{ useNewUrlParser: true })
+const client = new MongoClient(uri, { useNewUrlParser: true })
 
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
 const createColllection = (collectionName) => {
-    client.connect((err,db) => {
+    client.connect((err, db) => {
         projectCollection = client.db().collection(collectionName);
-        if(!err) {
+        if (!err) {
             console.log('MongoDB Connected')
         }
         else {
@@ -28,8 +27,8 @@ const createColllection = (collectionName) => {
     })
 }
 
-const insertProjects = (project,callback) => {
-    projectCollection.insert(project,callback);
+const insertProjects = (project, callback) => {
+    projectCollection.insert(project, callback);
 }
 
 const getProjects = (callback) => {
@@ -50,49 +49,49 @@ const cardList = [
         description: "Demo desciption about kitten 3"
     }
 ]
-app.get('/api/projects',(req,res) => {
-    getProjects((err,result) => {
-        if(err) {
-            res.json({statusCode: 400, message: err})
+app.get('/api/projects', (req, res) => {
+    getProjects((err, result) => {
+        if (err) {
+            res.json({ statusCode: 400, message: err })
         }
         else {
-            res.json({statusCode: 200, message:"Success", data: result})
+            res.json({ statusCode: 200, message: "Success", data: result })
         }
     })
-})       
+})
 
-app.post('/api/projects',(req,res) => {
+app.post('/api/projects', (req, res) => {
     console.log("New Project added", req.body)
     var newProject = req.body;
-    insertProjects(newProject,(err,result) => {
-        if(err) {
-            res.json({statusCode: 400, message: err})
+    insertProjects(newProject, (err, result) => {
+        if (err) {
+            res.json({ statusCode: 400, message: err })
         }
         else {
-            res.json({statusCode: 200, message:"Project Successfully added", data: result})
+            res.json({ statusCode: 200, message: "Project Successfully added", data: result })
         }
     })
 })
 const addNumbers = (number1, number2) => {
     var num1 = parseInt(number1)
     var num2 = parseInt(number2)
-    var result = num1 + num2;
+    var result = (num1 + num2) || null;
     return result;
 }
 
 app.get("/addTwoNumbers/:firstNumber/:secondNumber",(req,res) => {
-    var number1 = req.query.number1;
-    var number2 = req.query.number2;
-    var result = addNumbers(number1,number2)
+    var n1 = req.params.firstNumber;
+    var n2 = req.params.secondNumber;
+    var result = addNumbers(n1,n2)
     if(result == null) {
         res.json({result: result, statusCode: 400}).status(400)
       }
       else { res.json({result: result, statusCode: 200}).status(200) } 
-    })
+})
 
 var port = process.env.port || 3000;
 
-app.listen(port,()=>{
-    console.log("App running at http://localhost:"+port)
+app.listen(port, () => {
+    console.log("App running at http://localhost:" + port)
     createColllection("pets")
 })
